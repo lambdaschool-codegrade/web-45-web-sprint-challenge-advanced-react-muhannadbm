1d0ae56239a3494595cf4865874426774fafedc3
muhannadbm <mhd31145@hotmail.com>
2021-08-06 11:45:16 -0400

    custom hook and plantlist implemented

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+ const useForm = (initialValue) => {
+    const [values, setValues] = useState(initialValue)
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };
+      return [values,handleChanges,handleSubmit,showSuccessMessage]
+}
+export default useForm
